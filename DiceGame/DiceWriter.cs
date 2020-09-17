21f3c14b8b5a0d435eae685c2d9a63dc545efbcf
jorfus <71066116+jorfus@users.noreply.github.com>
2020-09-17 16:00:15 +0200

    Initial commit

---
+++ DiceGame/DiceWriter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DiceGame
+{
+    class DiceWriter
+    {
+        public void WriteDice(int[] randInts)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int column = 0; column < 120; column++)
+                {
+                    switch (row)
+                    {
+                        case 0:
+                        case 8:
+                            if ((column + 1) % 6 == 0)
+                                Console.Write(" ");
+                            else
+                                Console.Write("=");
+                            break;
+                        case 1:
+                        case 3:
+                        case 5:
+                            Console.Write(' ');
+                            break;
+                        case 2:
+                            switch (randInts[column / 6])
+                            {
+                                case 1:
+                                    Console.Write("|   | ");
+                                    break;
+                                case 2:
+                                case 3:
+                                    Console.Write("|  o| ");
+                                    break;
+                                case 4:
+                                case 5:
+                                case 6:
+                                    Console.Write("|o o| ");
+                                    break;
+                                default:
+                                    break;
+                            }
+                            column += 5;
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ DiceGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceWriter diceWrite = new DiceWriter();
+            Random rand = new Random();
+            int[] randInts = new int[20];
+            
+            while (true)
+            {
+                for (int i = 0; i < randInts.Length; i++)
+                {
+                    int randInt = rand.Next(1, 7);
+                    randInts[i] = randInt;
+                }
+
+                diceWrite.WriteDice(randInts);
+
+                foreach (int randInt in randInts)
+                    Console.Write($"{randInt} ");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
